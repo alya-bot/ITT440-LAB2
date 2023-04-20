e96e031129ae27ca0877673cb5035de85fffb998
alyabat <2021871656@student.uitm.edu.my>
2023-04-20 03:23:45 +0000

    commit for 2.3

--- 2.3.c
+++ 2.3.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/type.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-void childTask() {
-
- printf ("Salam, saya anak tau\n");
-}
-
-void parentTask() {
- printf ("Dan, saya adalah bapaknya \n");
-
-}
-
 int main (void) {
- pid_t pid = fork();
+	for (int i=1, i<13; i++) {
+		pid_t pid = fork();
+		
+		if (pid == 0) {
+			printf("Child process => PPID=%d, PID=%d\n", getppid(), getpid() );
+			exit(0);
+		}
 
-  if(pid = 0) {
-   childTask();
-   exit(EXIT_SUCCESS);
-  }
-  else if (pid>0) {
-   wait (NULL);
-   parentTask();
-  }
-  else  {
-   printf ("Unable to create child process.");
-  }
+		else {
+			printf("Parent process => PID=%d\n", getpid());
+			printf("Waiting for child processes to finish...\n");
+			wait(NULL);
+			printf("Child process finished.\n");
+		}
+	} 
 
 return EXIT_SUCCESS;
 
